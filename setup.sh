9c3e22796cb56261e8fae37a2974b2328572e72f
Tom Croux <47288189+CerfMetal@users.noreply.github.com>
2021-04-28 08:50:27 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#setup.sh
+sudo apt-get update  # Install the latest package lists
+sudo apt-get install python3.6 -y # Install python 3
+sudo apt-get install python3-pip -y # Install PIP for python3
+sudo apt-get install screen -y # Install the screen package (used to create virtual terminals)
+sudo apt-get install wget -y # Install the wget package (used to download the MineBot)
+sudo apt-get install unzip -y # Install the unzip package (used to unzip the MineBot)
+
+# Install all the necessary package for the minecraft discord bot
+pip3 install discord.py
+pip3 install asyncio
+pip3 install mcstatus
+pip3 install pyyaml 
+
+wget https://github.com/CerfMetal/Minecraft-Discord-Bot/archive/refs/heads/main.zip # Download the discord bot's files
+unzip main.zip
+
